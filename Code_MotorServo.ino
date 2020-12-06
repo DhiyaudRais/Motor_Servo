af050f7998f05eb152bc473826708fd5985d27f7
DhiyaudRais <72783971+DhiyaudRais@users.noreply.github.com>
2020-12-06 10:41:07 +0700

    Add files via upload

---
+++ Code_MotorServo.ino
@@ -0,0 +1,31 @@
+#include <Servo.h> 
+Servo servo1;              
+Servo servo2; 
+
+int sudut= 0;  
+ 
+void setup() 
+{ 
+servo1.attach(10);   
+                         
+servo2.attach(5);
+                        
+} 
+ 
+ void loop() 
+{ 
+
+for(sudut = 0; sudut < 180; sudut += 1) {
+ servo1.write(sudut); 
+ servo2.write(sudut);
+                            
+ delay(15);  
+} 
+ 
+
+for(sudut = 180; sudut>=1; sudut-=1){                                
+ servo1.write(sudut); 
+ servo2.write(sudut);
+ delay(15);
+  } 
+}
